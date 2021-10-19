e80209a186281a4bc2b048980bfcc1c0f665cb20
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2021-10-18 19:59:13 -0700

    Updated recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,10 @@ mkdir build
 pushd build
 
 
-cmake -GNinja -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib -DCMAKE_INSTALL_PREFIX=${PREFIX}  -DKALDI_BUILD_TEST=OFF  -DMATHLIB=OpenBLAS -DBLAS_LIBRARIES=${PREFIX}/lib/libopenblas${SHLIB_EXT} -DLAPACK_LIBRARIES=${PREFIX}/lib/libopenblas${SHLIB_EXT} ..
+cmake -GNinja -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib -DCMAKE_INSTALL_PREFIX=${PREFIX}  -DKALDI_BUILD_TEST=OFF  -DMATHLIB=OpenBLAS -DOVERRIDE_KALDI_VERSION=${PKG_VERSION} ..
 
 cmake --build . --target install --config Release -- -j ${CPU_COUNT}
+
+ls lib/ | grep ^libfst.*so$ | xargs -d"\n" rm #  Clean up libfst.so style files to prevent compatibility issues with openfst package
 
 popd
