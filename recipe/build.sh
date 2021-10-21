44f20c0a5b65e78c5428a23dc80bb8ca85989508
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2021-10-20 18:21:59 -0700

    Update windows compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,23 @@ cmake -GNinja -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_BUILD_TYPE=Release -DC
 
 cmake --build . --target install --config Release -- -j ${CPU_COUNT}
 
-ls ${PREFIX}/lib | grep ^libfst.*so$ | xargs -0 rm -v "{}" #  Clean up libfst.so style files to prevent compatibility issues with openfst package
+#  Clean up libfst.so style files to prevent compatibility issues with openfst package, Kaldi will be linked to libfst.so.16 or libfst.16.dylib
+
+rm -rf ${PREFIX}/lib/fst
+rm -rf ${PREFIX}/include/fst
+
+rm ${PREFIX}/lib/libfst${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstscript${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstcompact${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstconst${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstfar${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstfarscript${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstlinearscript${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstlookahead${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstmpdtscript${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstngram${SHLIB_EXT}
+rm ${PREFIX}/lib/libngram_fst${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstpdtscript${SHLIB_EXT}
+rm ${PREFIX}/lib/libfstspecial${SHLIB_EXT}
 
 popd
