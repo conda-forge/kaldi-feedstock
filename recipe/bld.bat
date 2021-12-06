cc725bfdae4698a009a3d46c995403464d4c67de
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2021-12-06 13:01:29 -0800

    Bump version and simplify cmake build
    Add export for headers and cuda compilation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,11 +4,10 @@ mkdir build
 
 cd build
 
-cmake -GNinja  -DOVERRIDE_KALDI_VERSION="%PKG_VERSION%" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR="%PREFIX%\Library\lib" ^
-                -DCONDA_ROOT="%BUILD_PREFIX%\Library" ^
-                 -DCMAKE_INSTALL_BINDIR="%PREFIX%\Library\bin" ^
-                -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"  ^
-                -DKALDI_BUILD_TEST=OFF  -DMATHLIB=MKL ..
+cmake -GNinja  -DOVERRIDE_KALDI_VERSION="%PKG_VERSION%" -DCMAKE_BUILD_TYPE=Release ^
+                -DCONDA_ROOT="%LIBRARY_PREFIX%" ^
+                -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"  ^
+                -DKALDI_BUILD_TEST=OFF ..
 
 cmake --build . --verbose --config Release -- -v -j %CPU_COUNT%
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,15 +5,15 @@ mkdir build
 pushd build
 
 
-cmake -GNinja -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
-              -DCONDA_ROOT="${BUILD_PREFIX}" \
-              -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_BINDIR=${PREFIX}/bin  -DKALDI_BUILD_TEST=OFF  -DMATHLIB=OpenBLAS \
-              -DOVERRIDE_KALDI_VERSION=${PKG_VERSION} ..
+cmake -GNinja ${CMAKE_ARGS} -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_BUILD_TYPE=Release \
+              -DCONDA_ROOT="${PREFIX}" \
+              -DCMAKE_INSTALL_PREFIX="${PREFIX}"   -DKALDI_BUILD_TEST=OFF \
+              -DOVERRIDE_KALDI_VERSION="${PKG_VERSION}" ..
 
 
 cmake --build . --verbose --config Release -- -v -j ${CPU_COUNT}
 
-cmake --install . --component conda_kaldi --verbose --config Release
+cmake --install . --component kaldi --verbose --config Release
 
 
 popd
