62d82a3d3413462c013e693f4dda0b655c137316
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2021-12-08 22:13:48 -0800

    Fix symbol references on mac (#18)
    Fixed linking with MKL dylibs on MacOSX
    Added tests for presence of header files

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,13 @@ mkdir build
 
 pushd build
 
-echo "${CMAKE_ARGS}"
+export LDFLAGS="$(echo $LDFLAGS | sed 's/-Wl,-dead_strip_dylibs//g')"
+export LDFLAGS_LD="$(echo $LDFLAGS_LD | sed 's/-dead_strip_dylibs//g')"
+export CXXFLAGS="$CXXFLAGS -Wno-deprecated-declarations"
+export CFLAGS="$CFLAGS -Wno-deprecated-declarations"
 
-cmake -GNinja ${CMAKE_ARGS} -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_BUILD_TYPE=Release \
+
+cmake -GNinja -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_BUILD_TYPE=Release \
               -DCONDA_ROOT="${PREFIX}" \
               -DCMAKE_INSTALL_PREFIX="${PREFIX}"   -DKALDI_BUILD_TEST=OFF \
               -DOVERRIDE_KALDI_VERSION="${PKG_VERSION}" ..
