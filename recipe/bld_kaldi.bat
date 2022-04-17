d2c88bf05d1a23f4e318f5764e91e852e47e9462
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2022-04-16 23:34:57 -0700

    Another attempt to fix cmake/windows incompatibility

--- recipe/bld_kaldi.bat
+++ recipe/bld_kaldi.bat
@@ -5,14 +5,6 @@ cd build
 
 if "%cuda_compiler_version%"=="None" (
     set USE_CUDA=0
-    cmake -GNinja ^
-        -DCMAKE_BUILD_TYPE=Release ^
-        -DCONDA_ROOT="%LIBRARY_PREFIX%" ^
-        -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-        -DKALDI_VERSION="%PKG_VERSION%" ^
-        -DBUILD_SHARED_LIBS=OFF ^
-        -DKALDI_BUILD_TEST=OFF ^
-        ..
 ) else (
     set USE_CUDA=1
     REM no cf-builds for NCCL on windows yet
@@ -21,19 +13,20 @@ if "%cuda_compiler_version%"=="None" (
     set USE_SYSTEM_NCCL=0
     set USE_STATIC_NCCL=0
     set USE_STATIC_CUDNN=0
+    set "CUDA_HOME=%CUDA_HOME:\=/%"
+    echo "%CUDA_HOME%"
+    set CUDA_TOOLKIT_ROOT_DIR=%CUDA_HOME%
     set MAGMA_HOME=%LIBRARY_PREFIX%
-
-    cmake -GNinja ^
-        -DCMAKE_BUILD_TYPE=Release ^
-        -DCONDA_ROOT="%LIBRARY_PREFIX%" ^
-        -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-        -DCUDA_TOOLKIT_ROOT_DIR="%CUDA_HOME%" ^
-        -DKALDI_VERSION="%PKG_VERSION%" ^
-        -DBUILD_SHARED_LIBS=OFF ^
-        -DKALDI_BUILD_TEST=OFF ^
-        ..
 )
 
+cmake -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCONDA_ROOT="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DKALDI_VERSION="%PKG_VERSION%" ^
+    -DBUILD_SHARED_LIBS=OFF ^
+    -DKALDI_BUILD_TEST=OFF ^
+    ..
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build . --verbose --config Release -- -v -j %CPU_COUNT%
