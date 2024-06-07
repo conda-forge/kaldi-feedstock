7ecc568b6d84c3a1bd370e7ff50963259bea3447
H. Vetinari <h.vetinari@gmx.com>
2024-06-07 15:00:18 +1100

    try CUDA_TOOLKIT_ROOT_DIR approach

--- recipe/build_kaldi.bat
+++ recipe/build_kaldi.bat
@@ -23,11 +23,7 @@ if "%cuda_compiler_version%"=="None" (
 )
 
 if "%cuda_compiler_version%"=="12.0" (
-    REM header-only on windows as of CUDA 12, see
-    REM https://github.com/conda-forge/cuda-nvtx-feedstock/issues/4
-    set "NvToolExt_INCLUDE_DIR=%LIBRARY_INC%/targets/x64"
-    REM set fake path to shut up CMake
-    set "NvToolExt_LIBRARIES=%LIBRARY_LIB%"
+    set "CMAKE_EXTRA=-DCUDA_TOOLKIT_ROOT_DIR=%LIBRARY_PREFIX%"
 )
 
 cmake -GNinja ^
@@ -37,6 +33,7 @@ cmake -GNinja ^
     -DKALDI_VERSION="%PKG_VERSION%" ^
     -DBUILD_SHARED_LIBS=ON ^
     -DKALDI_BUILD_TEST=OFF ^
+    !CMAKE_EXTRA! ^
     ..
 if %ERRORLEVEL% neq 0 exit 1
 
